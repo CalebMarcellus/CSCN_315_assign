12de11d2ead0271e23168c7abf7b7f69605ce9a0
CalebMarcellus <123421120+CalebMarcellus@users.noreply.github.com>
2025-02-25 04:52:07 -0500

    my files for project 2

--- Case_Project_2/proj2.js
+++ Case_Project_2/proj2.js
@@ -1,42 +1,18 @@
 const username = document.getElementById('name');
 const password = document.getElementById('password');
 const email = document.getElementById('email');
-const form = document.getElementById('form');
+const form = document.getElementById('profileform');
 const errorElement = document.getElementById('error');
 const mainContent = document.getElementById('profile-content');
 const age = document.getElementById('age');
+const errorMessage = document.getElementsByClassName('.error')
 
-form.addEventListener('submit', function() {
-    let messages = [];
-    if (password.Value.length <= 10){
-        messages.push('Please make a password at least 10 characters long');
-    }
-    else if ( password.Value === '')
-    {
 
-    }
-
-    if (username.Value === '')
-    {
-
-    }
-
-    if (email.Value === '')
-    {
-
-    }
-    if (messages.length > 0)
-    {   
-        
-        form.preventdefault();
-        let YearOfBirth = calculateDOB(2025, age);
-        errorElement.innerHTML = messages.join(', ');
-        mainContent.innerHTML = `
-        
-        `;
-    }
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
     
-})
+    validateinput();
+});
 
 function calculateDOB(currYear, userAge) {
 
@@ -44,11 +20,73 @@ let BirthYear = currYear - userAge;
 return BirthYear;
 }
 
- const setError = (element, message) =>
-{   const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querSelector('.error');
+ function setError(element, message)
+{   const formcontrol = element.parentElement;
+    const errorDisplay = formcontrol.querySelector('.error');
+    
+
+    errorDisplay.innerText = message;
+    //formcontrol.classlist.add('error');
+    //formcontrol.classlist.remove('success');
+}
+
+function setSuccess(element) {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
     errorDisplay.innertext = '';
-    inputControl.classlist.add('error');
-    inputControl.classlist.remove('success');
+    errorDisplay.innerHTML = '';
+    //inputControl.classlist.add('success');
+    //inputControl.classlist.remove('error');
+}
+
+function validateinput() {
+    const namevalue = username.value.trim();
+    const passwordValue = password.value.trim();
+    const emailValue = email.value.trim();
+    const agevalue = age.value.trim();
+
+
+    
+    if (namevalue === ''){
+        setError(password,'Password is a required field!');
+        console.error("No input provided.");
+        
+        
+    }
+    else if (passwordValue.length <= 10 && passwordValue.length > 0)
+    {
+        console.error("Invalid input provided for password.");
+        setError(password, 'Password must be at least 10 characters long');
+    }
+    else {
+        setSuccess(password);
+    }
+
+    if (namevalue === '')
+    {
+        setError(username, 'UserName is a required field');
+        console.error("No input provided.");
+    }
+    else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '')
+    {   console.error("No input provided.");
+        setError(email, 'Email is a Required field!');
+    }
+    else {
+        setSuccess(email);
+    }
+    if (agevalue === '')
+    {
+        console.error("No input provided.");
+        setError(age, 'Age is a required field!');
+    }
+    else{
+        setSuccess(age);
+    }
+
+    
 }
